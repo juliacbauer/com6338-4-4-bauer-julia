1b1ac55905507a91ec6bf9dfa20a9437aada130c
Julia Bauer <juliabauer@ufl.edu>
2023-06-10 21:31:07 -1000

    set up

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ var words = [
   'ukulele',
   'mango'
 ]
+
+// Set up vars
+var wordGuess = document.getElementById('word-to-guess')
+var remainingGuesses = document.getElementById('remaining-guesses')
+var winsEl = document.getElementById('wins')
+var lossesEl = document.getElementById('losses')
+var incorrectLetters = document.getElementById('incorrect-letters')
+var previousWord = document.getElementById('previous-word')
+
+var wins = 0
+var losses = 0
+var guesses = 10
+remainingGuesses.textContent = guesses
+
+// Pick random word
+var word = words[Math.floor(Math.random() * words.length)];
+console.log(word)
+
